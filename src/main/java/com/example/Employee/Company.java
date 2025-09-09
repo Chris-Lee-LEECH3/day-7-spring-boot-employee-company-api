21e5683d09a8eefbe79eb7888e09bc628066b6bb
LEECH3 <leech3@oocl.com>
2025-09-09 17:44:12 +0800

    feat: init company related files

---
+++ src/main/java/com/example/Employee/Company.java
@@ -0,0 +1,3 @@
+package com.example.Employee;
+
+public record Company(Integer id, String name) { }
---
+++ src/test/java/com/example/Employee/CompanyControllerTest.java
@@ -0,0 +1,18 @@
+package com.example.Employee;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CompanyControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private CompanyController companyController;
+
+}
